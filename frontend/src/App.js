a1134112baa40e39af2c0ec3101ad485b9cc1e57
Param <param23403@gmail.com>
2022-05-31 11:32:01 -0400

    db added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {React, useState} from 'react'
-import Login from '../components/Login'
+import Login from './components/Login'
 
 function App() {
   return (
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,9 @@
 import {React,useState} from "react"
 
 const Login = ()=>{
-
+return(
+<h1>This is the login page</h1>
+)
 }
  
 export default Login
